6b68222b74faf1d002cd02137eb2b4a54e07140d
LordFinn <finarfinofficiel@gmail.com>
2024-12-03 03:01:07 +0100

    Initial commit

---
+++ src/client/java/fr/lordfinn/steveparty/client/StevepartyClient.java
@@ -0,0 +1,10 @@
+package fr.lordfinn.steveparty.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StevepartyClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/fr/lordfinn/steveparty/Steveparty.java
@@ -0,0 +1,10 @@
+package fr.lordfinn.steveparty;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Steveparty implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
