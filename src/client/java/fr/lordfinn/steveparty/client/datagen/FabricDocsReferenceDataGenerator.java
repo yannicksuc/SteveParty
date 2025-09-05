f147cff35885ea8e63480cf52fdc701530602fbe
LordFinn <finarfinofficiel@gmail.com>
2025-09-05 03:11:15 +0200

    feat(datagen): add block loot table generation support
    This update introduces a new data generation feature for block loot tables, allowing blocks to drop themselves when broken. The implementation includes the generation of loot tables for various switcher block types and a new data generation entry point.
    
    Closes: #123 (example issue number)

---
+++ src/client/java/fr/lordfinn/steveparty/client/datagen/FabricDocsReferenceDataGenerator.java
@@ -0,0 +1,12 @@
+package fr.lordfinn.steveparty.client.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FabricDocsReferenceDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+        pack.addProvider(ModBlockLootTableProvider::new);
+    }
+}
---
+++ src/client/java/fr/lordfinn/steveparty/client/datagen/ModBlockLootTableProvider.java
@@ -0,0 +1,30 @@
+package fr.lordfinn.steveparty.client.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import fr.lordfinn.steveparty.blocks.ModBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.resource.featuretoggle.FeatureFlags;
+
+import java.util.Collections;
+import java.util.concurrent.CompletableFuture;
+
+import static fr.lordfinn.steveparty.blocks.ModBlocks.SWITCHER_BLOCKS;
+
+public class ModBlockLootTableProvider extends FabricBlockLootTableProvider {
+
+    protected ModBlockLootTableProvider(FabricDataOutput dataOutput, CompletableFuture<RegistryWrapper.WrapperLookup> registryLookup) {
+        super(dataOutput, registryLookup);
+    }
+
+    @Override
+    public void generate() {
+        // Make blocks drop themselves
+        addDrop(ModBlocks.HOP_SWITCH);
+        for (Block switcher : SWITCHER_BLOCKS)
+            addDrop(switcher);
+
+        // etc, for all your blocks
+    }
+}
