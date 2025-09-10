33a44f72cae576b8dbf9fe7ce54c326b1dea5701
LordFinn <finarfinofficiel@gmail.com>
2025-09-11 00:01:05 +0200

    refactor(renderer): clean up StencilItemRenderer code
    Removed commented-out code and unnecessary transformations for clarity. This improves the readability of the StencilItemRenderer class without changing its functionality.

--- src/client/java/fr/lordfinn/steveparty/client/items/StencilItemRenderer.java
+++ src/client/java/fr/lordfinn/steveparty/client/items/StencilItemRenderer.java
@@ -6,7 +6,6 @@ import fr.lordfinn.steveparty.items.custom.StencilItem;
 import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry.DynamicItemRenderer;
 
 import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.model.json.ModelTransformation;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ModelTransformationMode;
@@ -23,7 +22,6 @@ public class StencilItemRenderer implements DynamicItemRenderer {
         if (textures == null || textures.metalStencil() == null) return;
 
         matrices.push();
-        //matrices.scale(0.75f, 0.75f, 0.75f);
         matrices.translate(0, 0, 0);
 
         Identifier textureId = textures.metalStencil();
@@ -39,14 +37,9 @@ public class StencilItemRenderer implements DynamicItemRenderer {
                 (s) -> {
                     MatrixStack.Entry entry = s.peek();
                     Matrix4f matrix = entry.getPositionMatrix();
-                    //matrix.rotate((float) Math.toRadians(180f), 0, 1, 0);
-                    //matrix.rotate((float) Math.toRadians(-90f), 1, 0, 0);
-                    // Common base rotation
-                    // Additional transformations per mode
                     switch (mode) {
                         case GUI -> {
                             matrix.translate(0f, 0.5f, 0f);
-                            //matrix.scale(0.88f);
                             matrix.rotate((float) Math.toRadians(180f), 0, 1, 0);
                             matrix.rotate((float) Math.toRadians(-90f), 1, 0, 0);
                         }
@@ -61,7 +54,6 @@ public class StencilItemRenderer implements DynamicItemRenderer {
                             matrix.rotate((float) Math.toRadians(80f), 1, 0, 0);
                             matrix.rotate((float) Math.toRadians(190f), 0, 1, 0);
                             matrix.rotate((float) Math.toRadians(5f), 0, 0, 1);
-                            //matrix.rotate((float) Math.toRadians(60f), 1, 0, 0);
                         }
                         case THIRD_PERSON_RIGHT_HAND, THIRD_PERSON_LEFT_HAND -> {
                             matrix.translate(0f, 0.77f, -0.05f);
