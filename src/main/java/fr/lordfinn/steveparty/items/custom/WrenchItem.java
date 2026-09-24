84ed1675f5c50655cf25b220841d180eb0025d75
LordFinn <finarfinofficiel@gmail.com>
2026-09-24 02:25:29 +0200

    feat(wrench): take plastic pieces apart in one hit
    The wrench mines switcher blocks and plastic studs instantly (the block still
    drops), even while jumping or underwater. Other blocks are unchanged.

--- src/main/java/fr/lordfinn/steveparty/items/custom/WrenchItem.java
+++ src/main/java/fr/lordfinn/steveparty/items/custom/WrenchItem.java
@@ -1,5 +1,7 @@
 package fr.lordfinn.steveparty.items.custom;
 
+import fr.lordfinn.steveparty.blocks.custom.PlotBlock;
+import fr.lordfinn.steveparty.blocks.custom.SwitchyBlock;
 import fr.lordfinn.steveparty.blocks.custom.boardspaces.BoardSpaceBlockEntity;
 import fr.lordfinn.steveparty.blocks.custom.boardspaces.TileBlock;
 import fr.lordfinn.steveparty.components.BlockOriginComponent;
@@ -7,6 +9,8 @@ import fr.lordfinn.steveparty.components.DestinationsComponent;
 import fr.lordfinn.steveparty.components.ModComponents;
 import fr.lordfinn.steveparty.items.custom.cartridges.CartridgeItem;
 import fr.lordfinn.steveparty.utils.MessageUtils;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
@@ -21,8 +25,19 @@ import org.jetbrains.annotations.Nullable;
 
 public class WrenchItem extends AbstractDestinationsSelectorItem implements CartridgeContainerOpener {
 
+    // Instant break needs speed / hardness / 30 >= 1, even when the /5 airborne or underwater penalty applies
+    private static final float PLASTIC_MINING_SPEED = 1000f;
+
     public WrenchItem(Settings settings) {
         super(settings);
+    }
+
+    // The wrench takes plastic pieces (switcher blocks, studs) apart in one hit
+    @Override
+    public float getMiningSpeed(ItemStack stack, BlockState state) {
+        Block block = state.getBlock();
+        if (block instanceof SwitchyBlock || block instanceof PlotBlock) return PLASTIC_MINING_SPEED;
+        return super.getMiningSpeed(stack, state);
     }
 
     @Override
